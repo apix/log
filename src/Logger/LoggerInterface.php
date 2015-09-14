22e14b64faf1e7c957e452b46bb12ec3b0ba172f
Franck Cassedanne <franck@cassedanne.com>
2015-09-14 14:48:22 +0100

    Update LoggerInterface.php
    [skip ci]

--- src/Logger/LoggerInterface.php
+++ src/Logger/LoggerInterface.php
@@ -1,7 +1,22 @@
 <?php
 
+/**
+ *
+ * This file is part of the Apix Project.
+ *
+ * (c) Franck Cassedanne <franck at ouarz.net>
+ *
+ * @license     http://opensource.org/licenses/BSD-3-Clause  New BSD License
+ *
+ */
+
 namespace Apix\Log\Logger;
 
+/**
+ * Logger Interface.
+ *
+ * @author Franck Cassedanne <franck at ouarz.net>
+ */
 interface LoggerInterface
 {
 
